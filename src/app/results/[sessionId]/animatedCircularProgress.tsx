559439526e0dc6321d6576a99671f87c3cb8e15f
Victoran0 <victoranosunkvic@gmail.com>
2025-05-20 23:32:31 +0100

    Create AnimateCircularProgress component to show candidate's result

---
+++ src/app/results/[sessionId]/animatedCircularProgress.tsx
@@ -0,0 +1,48 @@
+"use client"
+import React, { useEffect, useState } from "react";
+
+const AnimatedCircularProgress = ({ percentage }: { percentage: number }) => {
+    const [displayedValue, setDisplayedValue] = useState(0);
+
+    useEffect(() => {
+        let start: number | null = null;
+        const duration = 1000; // 1 second
+        const target = percentage;
+        const animate = (timestamp: number) => {
+        if (!start) start = timestamp;
+        const progress = Math.min((timestamp - start) / duration, 1);
+        setDisplayedValue(Math.floor(progress * target));
+            if (progress < 1) {
+                requestAnimationFrame(animate);
+            }
+        };
+        requestAnimationFrame(animate);
+    }, [percentage]);
+
+    return (
+        <div className="relative w-32 h-32 mx-auto">
+            <svg className="w-full h-full" viewBox="0 0 36 36">
+                <path className="text-muted/40" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" stroke="currentColor" strokeWidth="3" ></path>
+                <path
+                    className="text-primary"
+                    strokeDasharray={`${displayedValue}, 100`}
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    style={{ transition: "stroke-dasharray 0.1s ease-in-out" }}
+                ></path>
+            </svg>
+            <div className="absolute inset-0 flex items-center justify-center">
+                <span className="text-3xl font-bold text-primary">
+                    {displayedValue}%
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default AnimatedCircularProgress;
