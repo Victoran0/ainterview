06b0cff6d8bedaccab317a9c0ade806befbd763f
Victoran0 <victoranosunkvic@gmail.com>
2025-05-27 04:35:49 +0100

    Create generateLocalID to track the ids for resume editing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const generateLocalId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
